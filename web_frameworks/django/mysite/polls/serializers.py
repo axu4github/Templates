9d8d7c3c02726f210decc1df2c017122d6c6efb2
anxu <axu.home@gmail.com>
2018-11-12 14:18:52 +0800

    测试REST-Framework接口

--- web_frameworks/django/mysite/polls/serializers.py
+++ web_frameworks/django/mysite/polls/serializers.py
@@ -6,14 +6,14 @@ from rest_framework import serializers
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
-        fields = ('question_text', 'pub_date')
+        fields = ('id', 'question_text', 'pub_date')
 
 
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
-    question = serializers.HyperlinkedRelatedField(
-        many=True, view_name='polls:detail',
-        read_only=True, lookup_field='username')
+    # questions = serializers.HyperlinkedRelatedField(
+    #     many=True, view_name='polls:detail',
+    #     read_only=True, lookup_field='username')
 
     class Meta:
         model = Choice
-        fields = ('question', 'choice_text', 'votes')
+        fields = ('choice_text', 'votes')
